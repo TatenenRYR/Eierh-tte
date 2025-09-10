d2feb028c72ca651309bdc6cd80aefe8a4f74da5
Tatenen <marvin-15@hotmail.de>
2025-09-11 00:03:15 +0200

    Update ors-proxy.php nvg

--- ors-proxy.php
+++ ors-proxy.php
@@ -1,18 +1,16 @@
 <?php
-// CORS erlauben
 header("Access-Control-Allow-Origin: *");
 header("Content-Type: application/json");
 
-// OpenRouteService Endpoint
 $url = "https://api.openrouteservice.org/v2/directions/cycling-regular/json";
+$apiKey = "eyJvcmciOiI1YjNjZTM1OTc4NTExMTAwMDFjZjYyNDgiLCJpZCI6ImRkZWI3Y2EzZWU1ZTUzYmRiNTZkZGQzYTQ4MmQ1NzkyZGJiZGM5OGRmMDQ4ZTU3YWMwODYwNzEwIiwiaCI6Im11cm11cjY0In0="; // <--- richtigen ORS-Key eintragen
 
-// Request vom Client weiterleiten
 $input = file_get_contents("php://input");
 
 $ch = curl_init($url);
 curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
 curl_setopt($ch, CURLOPT_HTTPHEADER, [
-    "Authorization: eyJvcmciOiI1YjNjZTM1OTc4NTExMTAwMDFjZjYyNDgiLCJpZCI6ImRkZWI3Y2EzZWU1ZTUzYmRiNTZkZGQzYTQ4MmQ1NzkyZGJiZGM5OGRmMDQ4ZTU3YWMwODYwNzEwIiwiaCI6Im11cm11cjY0In0=",
+    "Authorization: $apiKey",
     "Content-Type: application/json"
 ]);
 curl_setopt($ch, CURLOPT_POST, true);
@@ -20,6 +18,12 @@ curl_setopt($ch, CURLOPT_POSTFIELDS, $input);
 
 $response = curl_exec($ch);
 $httpCode = curl_getinfo($ch, CURLINFO_HTTP_CODE);
+
+// Debug ins Server-Log
+error_log("ORS Proxy Request Body: " . $input);
+error_log("ORS Proxy Response Code: " . $httpCode);
+error_log("ORS Proxy Response: " . $response);
+
 curl_close($ch);
 
 http_response_code($httpCode);
